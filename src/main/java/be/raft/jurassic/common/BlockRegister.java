9027746a76df4d090e1e16447ad1c801d3c9a8c3
Raft <72615320+Raft08@users.noreply.github.com>
2022-10-09 11:21:16 +0200

    Added Geckolib a dependecy

--- src/main/java/be/raft/jurassic/common/BlockRegister.java
+++ src/main/java/be/raft/jurassic/common/BlockRegister.java
@@ -1,11 +1,31 @@
 package be.raft.jurassic.common;
 
 import be.raft.jurassic.Jurassic;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.block.Block;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 
 public class BlockRegister {
+    //Call this to correctly register all blocks
     public static void register() {
        Jurassic.logger.info("Why does the ground rumble like this?");
     }
 
+    //Registers
+        //Block Register
+    private static Block registerBlock(String name, Block block, ItemGroup group) {
+        registerBlockItems(name, block, group);
+        return Registry.register(Registry.BLOCK,new Identifier(Jurassic.modId, name), block);
+    }
+        //ItemBlock Register
+    private static Item registerBlockItems(String name, Block block, ItemGroup group){
+        return Registry.register(Registry.ITEM, new Identifier(Jurassic.modId, name),
+                new BlockItem(block, new FabricItemSettings().group(group)));
+    }
 
+    //Blocks
 }
--- src/main/java/be/raft/jurassic/common/ItemRegister.java
+++ src/main/java/be/raft/jurassic/common/ItemRegister.java
@@ -1,9 +1,21 @@
 package be.raft.jurassic.common;
 
 import be.raft.jurassic.Jurassic;
+import net.minecraft.item.Item;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 
 public class ItemRegister {
+    //Call this to correctly register all items
     public static void register() {
         Jurassic.logger.info("NOO!!! *Lost Connection*");
     }
+
+    //Registers
+        //Item Register
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registry.ITEM, new Identifier(Jurassic.modId, name), item);
+    }
+
+    //Items
 }
